a55f0eeb83e46ebf803548251c90ccced142e0ba
Anchu Antony <anchuantony2212@gmail.com>
2024-05-20 23:22:25 +0200

    Add project files

---
+++ VendingMachine/VendingMachine/IVending.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal interface IVending
+    {
+        Product Purchase(int productId);
+        List<string> ShowAll();
+        string Details(int productId);
+        void InsertMoney(int amount);
+        Dictionary<int, int> EndTransaction();
+    }
+}
---
+++ VendingMachine/VendingMachine/VendingMachineService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class VendingMachineService : IVending
+    {
+        public string Details(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, int> EndTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertMoney(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Purchase(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ShowAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
